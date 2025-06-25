63ce98af7978e683423a51d723c187982a50560c
viih_pinafo <143670429+viihpinafo@users.noreply.github.com>
2025-06-25 20:59:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo código");
+
+let chute;
+let idade;
+let nome;
+
+console.log(numero);
+
+// Alerta simples quando o site carregar
+window.onload = function () {
+  alert("Bem-vindo à nossa loja de produtos religiosos!");
+};
+
+let entrada = prompt("Digite um número inteiro:");
+let numero = parseInt(entrada);
+
+if (Number.isInteger(numero)) {
+  console.log("Obrigado! Você digitou um inteiro.");
+  alert("Obrigado! Você digitou um inteiro.");
+} else {
+  console.log("Por favor, digite um número inteiro válido.");
+  alert("Por favor, digite um número inteiro válido.");
+}
+
+alert('Seja bem-vindo ao nosso jogo!');
+
+// Gera um número secreto aleatório entre 1 e 10
+let numeroSecreto = 5;
+console.log(numeroSecreto); // (Para testes, pode remover depois)
+
+// Variável para armazenar o chute do usuário
+let chute;
+let tentativas = 0; // Inicia o contador de tentativas
+
+while (chute != numeroSecreto) {
+  chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+  tentativas++; // Soma +1 a cada tentativa
+
+  if (chute == numeroSecreto) {
+    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é menor que ${chute}. Tente novamente.`);
+  } else {
+    alert(`O número secreto é maior que ${chute}. Tente novamente.`);
+  }
+}
 
